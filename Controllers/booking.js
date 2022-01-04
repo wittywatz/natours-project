43fcc5831b49339568f9b372fbb867374debf140
wittywatz <watson.agbramu@neofinancial.com>
2022-01-04 16:02:07 -0500

    debugging stripe webhook

--- Controllers/booking.js
+++ Controllers/booking.js
@@ -53,8 +53,7 @@ exports.webhookCheckout = (req, res, next) => {
   } catch (err) {
     return res.status(400).send(`Webhook error: ${err.message}`);
   }
-  if (event.type === 'checkout.session.completed')
-    createBookingCheckout(event.data.object);
+  if (event.type === 'checkout.session.completed') createBookingCheckout(event);
 
   res.status(200).json({ received: true });
 };
