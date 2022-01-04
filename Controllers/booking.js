b33fc33451717ae8b6556746df04c104cb456ab2
wittywatz <watson.agbramu@neofinancial.com>
2022-01-04 16:14:52 -0500

    debugging stripe webhook

--- Controllers/booking.js
+++ Controllers/booking.js
@@ -36,7 +36,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.display_items[0].amount / 100;
+  const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
 };
 
@@ -53,8 +53,8 @@ exports.webhookCheckout = (req, res, next) => {
   } catch (err) {
     return res.status(400).send(`Webhook error: ${err.message}`);
   }
-  console.log(event);
-  if (event.type === 'checkout.session.completed') createBookingCheckout(event);
+  if (event.type === 'checkout.session.completed')
+    createBookingCheckout(event.data.object);
 
   res.status(200).json({ received: true });
 };
